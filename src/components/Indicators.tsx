e614c1469803d39e6f4aa0679d02d9f6cc2b6e0e
sweetcapucino <thuylinh271055@gmail.com>
2021-12-27 17:39:38 +0700

    commit

---
+++ src/components/Indicators.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function Indicators() {
+    return 
+        <div>
+            
+        </div>;
+}
---
+++ src/stories/Indicators.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta} from '@storybook/react';
+
+import { Indicators } from '../components/Indicators';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Indicators',
+  component: Indicators,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//   },
+};
